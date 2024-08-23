17588b4812517c69c38869cb3644d2a76e5ac7d9
Reisel <reiselmillan@gmail.com>
2024-08-23 22:25:03 +0200

    moved some calcs and cvs to nff.io.ase

---
+++ nff/io/colvars.py
@@ -0,0 +1,318 @@
+
+import torch
+import numpy as np
+
+def get_cv_from_dic(val, device="cpu"):
+    if val["type"].lower() == "pvp":
+        mol_inds = val["mol_idx"]  # caution check type
+        ring_inds = val["ring_idx"]
+        cv = ProjVectorPlane(mol_inds, ring_inds)
+    elif val["type"].lower() == "pvv":
+        mol_inds = val["mol_idx"]
+        reference = val['ref_idx']
+        vector = val["vector"]
+        cv = ProjVectorVector(vector, mol_inds, reference, device=device) # z components
+    elif val["type"].lower() == "proj_ortho_vectors_plane":
+        mol_inds = [i-1 for i in val["mol_idx"]]  # caution check type
+        ring_inds = [i-1 for i in val["ring_idx"]]
+        cv = ProjOrthoVectorsPlane(mol_inds, ring_inds) # z components
+    elif val["type"].lower() == "torsion":
+        cv = Dihedral(val["idx"], device)
+    elif val["type"].lower() == "distance":
+        cv = Distance(val["idx"], device)
+    elif val["type"].lower() in ["center_wall", "wall_center"]:
+        cv = CenterCageWall(val["mol_idx"], val["center_idx"], val["radius"], device=device)
+    elif val["type"].lower() in ["diff_distance2"]:
+        cv = DiffDistance2(val["idx"], device)
+    else:
+        raise TypeError("Bad CV type")
+    return cv
+
+
+class CenterCageWall:
+    def __init__(self, mol_idx=[], cage_idx=[], radius=0,  device='cpu'):
+        self.mol_idx = torch.LongTensor(mol_idx)
+        self.cage_idx = torch.LongTensor(cage_idx)
+        self.radius = float(radius)
+        self.device = device
+
+    def get_value(self, positions):
+        if not isinstance(positions, torch.Tensor):
+            positions = torch.tensor(positions)
+        mol_pos = positions[self.mol_idx]
+        cage_pos = positions[self.cage_idx]
+        mol_centroid = mol_pos.mean(axis=0) # mol center
+        cage_centroid = cage_pos.mean(axis=0) # centroid of the whole structure
+
+        dist = mol_centroid - cage_centroid
+        dist = torch.linalg.norm(dist)
+        if dist > self.radius:
+            return dist - self.radius
+        else:
+            return 0
+        
+
+class ProjVectorVector:
+    """
+    Collective variable class. Projection of a position vector onto a reference vector
+    Atomic indices are used to determine the coordiantes of the vectors.
+    Params
+    ------
+    vector: list of int
+       List of the indices of atoms that define the vector on which the position vector is projected
+    indices: list if int
+       List of indices of the mol/fragment
+    reference: list of int
+       List of atomic indices that are used as reference for the position vector
+
+    note: the position vector is calculated in the method get_value
+    """
+    def __init__(self, vector=[], indices=[], reference=[], device='cpu'):
+        self.vector = torch.tensor([float(i) for i in vector], device=device)
+        self.vector = self.vector / torch.linalg.norm(self.vector) # normalize
+        self.mol_inds = torch.LongTensor(indices)
+        self.reference_inds = reference
+        self.device = device
+
+    def get_value(self, positions):
+        mol_pos = positions[self.mol_inds]
+        reference_pos = positions[self.reference_inds]
+        mol_centroid = mol_pos.mean(axis=0) # mol center
+        reference_centroid = reference_pos.mean(axis=0) # centroid of the whole structure
+
+        # position vector with respect to the structure centroid
+        rel_mol_pos = mol_centroid - reference_centroid
+
+        # projection
+        cv = torch.dot(rel_mol_pos.to(torch.float32), self.vector.to(torch.float32))
+        return cv
+
+class ProjVectorCentroid2:
+    """
+    Collective variable class. Projection of a position vector onto a reference vector
+    Atomic indices are used to determine the coordiantes of the vectors.
+    Params
+    ------
+    vector: list of int
+       List of the indices of atoms that define the vector on which the position vector is projected
+    indices: list if int
+       List of indices of the mol/fragment
+    reference: list of int
+       List of atomic indices that are used as reference for the position vector
+
+    note: the position vector is calculated in the method get_value
+    """
+    def __init__(self, vector=[], indices=[], reference=[], device='cpu'):
+        self.vector_inds = vector
+        self.mol_inds = torch.LongTensor(indices)
+        self.reference_inds = reference
+
+    def get_value(self, positions):
+        vector_pos = positions[self.vector_inds]
+        vector = vector_pos[1] - vector_pos[0]
+        vector = vector / torch.linalg.norm(vector)
+        mol_pos = positions[self.mol_inds]
+        reference_pos = positions[self.reference_inds]
+        mol_centroid = mol_pos.mean(axis=0) # mol center
+        reference_centroid = reference_pos.mean(axis=0) # centroid of the whole structure
+
+        # position vector with respect to the structure centroid
+        rel_mol_pos = mol_centroid - reference_centroid
+
+        # projection
+        cv = torch.dot(rel_mol_pos, vector)
+        return cv
+
+
+class ProjVectorPlane:
+    """
+    Collective variable class. Projection of a position vector onto a the average plane
+    of an arbitrary ring defined in the structure
+    Atomic indices are used to determine the coordiantes of the vectors.
+    Params
+    ------
+    mol_inds: list of int
+       List of indices of the mol/fragment tracked by the CV
+    ring_inds: list of int
+       List of atomic indices of the ring for which the average plane is calculated.
+
+    note: the position vector is calculated in the method get_value
+    """
+    def __init__(self, mol_inds = [], ring_inds = []):
+        self.mol_inds = torch.LongTensor(mol_inds) # list of indices
+        self.ring_inds = torch.LongTensor(ring_inds) # list of indices
+        # both self.mol_coors and self.ring_coors torch tensors with atomic coordinates
+        # initiallized as list but will be set to torch tensors with set_positions
+        self.mol_coors = []
+        self.ring_coors = []
+
+    def set_positions(self, positions):
+        # update coordinate torch tensors from the positions tensor
+        self.mol_coors = positions[self.mol_inds]
+        self.ring_coors = positions[self.ring_inds]
+
+    def get_indices(self):
+        return self.mol_inds + self.ring_inds
+
+    def get_value(self, positions):
+        """Calculates the values of the CV for a specific atomic positions
+
+        Args:
+            positions (torch tensor): atomic positions
+
+        Returns:
+            float: current values of the collective variable
+        """
+        self.set_positions(positions)
+        mol_cm = self.mol_coors.mean(axis=0) # mol center
+        ring_cm = self.ring_coors.mean(axis=0) # ring center
+        # ring atoms to center
+        self.ring_coors = self.ring_coors - ring_cm
+
+        r1 = torch.zeros(3, device=self.ring_coors.device)
+        N = len(self.ring_coors) # number of atoms in the ring
+        for i, rl0 in enumerate(self.ring_coors):
+            r1 = r1 + rl0 * np.sin(2 * np.pi * i / N)
+        r1 = r1/N
+
+        r2 = torch.zeros(3, device=self.ring_coors.device)
+        for i, rl0 in enumerate(self.ring_coors):
+            r2 = r2 + rl0 * np.cos(2 * np.pi * i / N)
+        r2 = r2/N
+
+        plane_vec = torch.cross(r1, r2)
+        plane_vec = plane_vec / torch.linalg.norm(plane_vec)
+        pos_vec = mol_cm - ring_cm
+
+        cv = torch.dot(pos_vec, plane_vec)
+        return cv
+
+
+class ProjOrthoVectorsPlane:
+    """
+    Collective variable class. Projection of a position vector onto a the average plane
+    of an arbitrary ring defined in the structure
+    Atomic indices are used to determine the coordiantes of the vectors.
+    Params
+    ------
+    mol_inds: list of int
+       List of indices of the mol/fragment tracked by the CV
+    ring_inds: list of int
+       List of atomic indices of the ring for which the average plane is calculated.
+
+    note: the position vector is calculated in the method get_value
+    """
+    def __init__(self, mol_inds = [], ring_inds = []):
+        self.mol_inds = torch.LongTensor(mol_inds) # list of indices
+        self.ring_inds = torch.LongTensor(ring_inds) # list of indices
+        # both self.mol_coors and self.ring_coors torch tensors with atomic coordinates
+        # initiallized as list but will be set to torch tensors with set_positions
+        self.mol_coors = []
+        self.ring_coors = []
+
+    def set_positions(self, positions):
+        # update coordinate torch tensors from the positions tensor
+        self.mol_coors = positions[self.mol_inds]
+        self.ring_coors = positions[self.ring_inds]
+
+    def get_indices(self):
+        return self.mol_inds + self.ring_inds
+
+    def get_value(self, positions):
+        """Calculates the values of the CV for a specific atomic positions
+
+        Args:
+            positions (torch tensor): atomic positions
+
+        Returns:
+            float: current values of the collective variable
+        """
+        self.set_positions(positions)
+        mol_cm = self.mol_coors.mean(axis=0) # mol center
+        ring_cm = self.ring_coors.mean(axis=0) # ring center
+        # ring atoms to center
+        self.ring_coors = self.ring_coors - ring_cm
+
+        r1 = torch.zeros(3, device=self.ring_coors.device)
+        N = len(self.ring_coors) # number of atoms in the ring
+        for i, rl0 in enumerate(self.ring_coors):
+            r1 = r1 + rl0 * np.sin(2 * np.pi * i / N)
+        r1 = r1/N
+
+        r2 = torch.zeros(3, device=self.ring_coors.device)
+        for i, rl0 in enumerate(self.ring_coors):
+            r2 = r2 + rl0 * np.cos(2 * np.pi * i / N)
+        r2 = r2/N
+
+        # normalize r1 and r2
+        r1 = r1 / torch.linalg.norm(r1)
+        r2 = r2 / torch.linalg.norm(r2)
+        # project position vector on r1 and r2
+        pos_vec = mol_cm - ring_cm
+        proj1 = torch.dot(pos_vec, r1)
+        proj2 = torch.dot(pos_vec, r2)
+        cv = proj1 + proj2
+        return abs(cv)
+
+
+class Distance:
+    def __init__(self, idx=[], device="cpu"):
+        self.idx = idx
+        self.device = device
+
+    def get_value(self, positions):
+        d = positions[self.idx[0]] - positions[self.idx[1]]
+        d2 = d*d
+        return torch.sqrt(d2.sum())
+
+
+class DiffDistance2:
+    def __init__(self, idx=[], device="cpu"):
+        self.idx = idx
+        self.device = device
+
+    def get_value(self, positions):
+        d1 = positions[self.idx[0]] - positions[self.idx[1]]
+        d2 = positions[self.idx[2]] - positions[self.idx[3]]
+        
+        d1_2 = d1*d1
+        d2_2 = d2*d2
+        return torch.sqrt(d2_2.sum()) - torch.sqrt(d1_2.sum())
+    
+
+class Dihedral:
+    """
+    Collective variable class. Projection of a position vector onto a reference vector
+    Atomic indices are used to determine the coordiantes of the vectors.
+    Params
+    ------
+    vector: list of int
+       List of the indices of atoms that define the vector on which the position vector is projected
+    indices: list if int
+       List of indices of the mol/fragment
+    reference: list of int
+       List of atomic indices that are used as reference for the position vector
+
+    note: the position vector is calculated in the method get_value
+    """
+    def __init__(self, idx=[], device="cpu"):
+      self.idx  = idx
+      self.device = device
+
+    def get_value(self, positions):
+      ba = -(positions[self.idx[0]] - positions[self.idx[1]])
+      bc = positions[self.idx[1]] - positions[self.idx[2]]
+      cd = positions[self.idx[3]] - positions[self.idx[2]]
+
+      ba = ba/torch.linalg.norm(ba)
+      bc = bc/torch.linalg.norm(bc)
+      cd = cd/torch.linalg.norm(cd)
+
+      n1 = torch.cross(ba, bc)
+      n2 = torch.cross(bc, cd)
+      m = torch.cross(n1, bc)
+      x = torch.dot(n1, n2)
+      y = torch.dot(m, n2)
+
+      ang = 180.0 / torch.pi * torch.arctan2(y, x)
+      return ang
---
+++ nff/io/restraints.py
@@ -0,0 +1,113 @@
+import torch
+from torch.autograd import grad
+from nff.io.colvars import get_cv_from_dic
+
+
+class HarmonicRestraint:
+    """Class to apply a harmonic restraint on a MD simulations
+    Params
+    ------
+    cvdic (dict): Dictionary contains the information to define the collective variables
+                  and the harmonic restraint.
+    max_steps (int): maximum number of steps of the MD simulation
+    device: device
+    """
+    def __init__(self, cvdic, max_steps, device="cpu"):
+        self.cvs = [] # list of collective variables (CV) objects
+        self.kappas = []  # list of constant values for every CV
+        self.eq_values = [] # list equilibrium values for every CV
+        self.steps = []  # list of lists with the steps of the MD simulation
+        self.device = device
+        self.setup_contraint(cvdic, max_steps, device)
+
+    def setup_contraint(self, cvdic, max_steps, device):
+        """ Initializes the collectiva variables objects
+        Args:
+            cvdic (dict): Dictionary contains the information to define the collective variables
+                          and the harmonic restraint.
+            max_steps (int): maximum number of steps of the MD simulation
+            device: device
+        """
+        for _, val in cvdic.items():
+            cv = get_cv_from_dic(val, device)
+            self.cvs.append(cv)
+            steps, kappas, eq_values = self.create_time_dependec_arrays(val["restraint"], max_steps)
+            self.kappas.append(kappas)
+            self.steps.append(steps)
+            self.eq_values.append(eq_values)
+
+    def create_time_dependec_arrays(self, restraint_list, max_steps):
+        """creates lists of steps, kappas and equilibrium values that will be used along
+        the simulation to determine the value of kappa and equilibrium CV at each step
+
+        Args:
+            restraint_list (list of dicts): contains dictionaries with the desired values of
+                                            kappa and CV at arbitrary step in the simulation
+            max_steps (int): maximum number of steps of the simulation
+
+        Returns:
+            list: all steps e.g [1,2,3 .. max_steps]
+            list: all kappas for every step, e.g [0.5, 0.5, 0.51, .. ] same size of steps
+            list: all equilibrium values for every step, e.g. [1,1,1,3,3,3, ...], same size of steps
+        """
+        steps = []
+        kappas = []
+        eq_vals = []
+        # in case the restraint does not start at 0
+        templist = list(range(0, restraint_list[0]['step']))
+        steps += templist
+        kappas += [0 for _ in templist]
+        eq_vals += [0 for _ in templist]
+
+        for n, rd in enumerate(restraint_list[1:]):
+            # rd and n are out of phase by 1, when n = 0, rd points to index 1
+            templist = list(range(restraint_list[n]['step'], rd['step']))
+            steps += templist
+            kappas += [restraint_list[n]['kappa'] for _ in templist]
+            dcv = rd['eq_val'] - restraint_list[n]['eq_val']
+            cvstep = dcv/len(templist) # get step increase
+            eq_vals += [restraint_list[n]['eq_val'] + cvstep * tind for tind, _ in enumerate(templist)]
+
+        # in case the last step is lesser than the max_step
+        templist = list(range(restraint_list[-1]['step'], max_steps))
+        steps += templist
+        kappas += [restraint_list[-1]['kappa'] for _ in templist]
+        eq_vals += [restraint_list[-1]['eq_val'] for _ in templist]
+
+        return steps, kappas, eq_vals
+
+    def get_energy(self, positions, step):
+        """ Calculates the retraint energy of an arbritrary state of the system
+        Args:
+            positions (torch.tensor): atomic positions
+            step (int): current step
+        Returns:
+            float: energy
+        """
+        tot_energy = 0
+        for n, cv in enumerate(self.cvs):
+            kappa = self.kappas[n][step]
+            eq_val = self.eq_values[n][step]
+            cv_value = cv.get_value(positions)
+            energy = 0.5 * kappa * (cv_value - eq_val) * (cv_value - eq_val)
+            tot_energy += energy
+        return tot_energy
+
+    def get_bias(self, positions, step):
+        """ Calculates the bias energy and force
+
+        Args:
+            positions (torch.tensor): atomic positions
+            step (int): current step
+
+        Returns:
+            float: forces
+            float: energy
+        """
+        if len(self.cvs) == 0:
+            return torch.tensor(0), torch.tensor(0)
+        energy = self.get_energy(positions, step)
+        forces = -grad(energy, positions)[0]
+        return forces, energy
+
+
